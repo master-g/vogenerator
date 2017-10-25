840622fecb82e9826c3adcfc42b3f114c3123647
mg <master-g@users.noreply.github.com>
2017-10-25 16:31:59 +0800

    update

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#protoc --plugin=protoc-gen-vo=vo-generator.py --vo_out=./build hello.proto
-protoc -I=sample --plugin=protoc-gen-vo=protoc-gen-vo --vo_out=./sample/build sample/hello.proto
